2e3de96f1658a5b79003f756e33d656ef107a41b
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-15 10:30:38 +0200

    :arrow_up: Added Html

---
+++ Component.js
@@ -0,0 +1,12 @@
+Component class{
+  parentNode;
+  element;
+  tag;
+  className;
+constructor (){
+  this.parentNode = parentNode;
+  this.element = element;
+this.container = document.createElement(tag);
+}
+
+}
